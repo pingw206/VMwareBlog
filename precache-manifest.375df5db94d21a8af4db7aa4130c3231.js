53d94337acd00d1b6d7a6614fcb3d89fd80e87e3
pingw206 <pingw206@gmail.com>
2020-06-08 16:17:08 +0800

    Updates

---
+++ precache-manifest.375df5db94d21a8af4db7aa4130c3231.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7288fb4c521c7d535145bc302bda3ef2",
+    "url": "/vmware-blog/index.html"
+  },
+  {
+    "revision": "774f24efbe2dd0de5927",
+    "url": "/vmware-blog/static/js/2.2e88af13.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vmware-blog/static/js/2.2e88af13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b8d489d54f31ff79500",
+    "url": "/vmware-blog/static/js/3.66748433.chunk.js"
+  },
+  {
+    "revision": "fbddafb4f2e9b7a35998",
+    "url": "/vmware-blog/static/js/main.ad25d398.chunk.js"
+  },
+  {
+    "revision": "0062b10a45f7e76d7579",
+    "url": "/vmware-blog/static/js/runtime-main.b4b1d7dc.js"
+  },
+  {
+    "revision": "e42d06434a387763e1d1eb3243fde122",
+    "url": "/vmware-blog/static/media/vm-banner.e42d0643.jpeg"
+  }
+]);
